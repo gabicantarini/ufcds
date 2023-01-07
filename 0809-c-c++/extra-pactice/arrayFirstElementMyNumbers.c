cf03b1fce9ec209a237b2c6aa5e1ed9bece52d4c
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-01-07 15:42:12 +0000

    added first element

---
+++ 0809-c-c++/extra-pactice/arrayFirstElementMyNumbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//Print the value of the second element in the myNumbers array.
+int main() {
+
+    
+    int myNumbers[] = {25, 50, 75, 100};
+
+
+    myNumbers[0] = 33;
+    
+    printf("%d", myNumbers[0]);
+
+ return 0;
+
+}
