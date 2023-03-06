3d843549883a5177a600840e04e640bcf594ae0d
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-03-06 23:18:52 +0000

    added airport info

--- 0809-c-c++/aeroporto.c
+++ 0809-c-c++/aeroporto.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main() {
+    int horaPartida;
+   
+
+    // Solicitar a hora de partida
+    printf("AEROPORTO: INFORMAÇÕES DE EMBARQUE!\n");
+    printf("Digite a hora de partida (0-23): ");
+    scanf("%d", &horaPartida);
+
+    // Validar a hora de partida
+    if (horaPartida < 0 || horaPartida > 23) {
+        printf("Hora de partida inválida.\n");
+        return 0;
+    }
+
 
 
     return 0;
