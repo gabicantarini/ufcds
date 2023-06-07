09cc022e0f6e70077f6df3caa31a4eae90ddfd76
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-06-07 16:39:00 +0100

    added second element

---
+++ 0809-c-c++/extra-pactice/arraySecondMyNumbers copy.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//Print the value of the second element in the myNumbers array.
+int main() {
+
+    
+    int myNumbers[] = {25, 50, 75, 100};
+    
+    printf("%d", myNumbers[1]);
+
+ return 0;
+
+}
