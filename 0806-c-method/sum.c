aabba3243cc8c1bd92c20bc2013d408453455ed3
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-06-18 10:31:42 +0100

    added sum in c

--- 0806-c-method/sum.c
+++ 0806-c-method/sum.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
 
+int main()
+{
+    int j, k;
+    
+    printf("dê um valor: ");
+    scanf("%d",&j);
+    printf("dê outro valor: ");
+    scanf("%d",&k);
+  
+    printf("%d\n",j+k);
+    
+    return 0;
+}
