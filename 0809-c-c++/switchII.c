fb7a1adf8310f034d918d20f138c127b7861a8f7
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-07-08 00:37:51 +0100

    added switchCase days

---
+++ 0809-c-c++/switchII.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    int day = 4;
+    switch (day) {
+    
+    case  1:
+        printf("Saturday");
+        
+    break;
+    
+    case  2:
+        printf("Sunday");
+        
+    break;
+    
+    default:
+        printf("Weekend");
+    }
+
+    return 0;
+}
+
