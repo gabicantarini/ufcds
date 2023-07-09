6f34eecbd3e8913bdceba38aad6f29e8735dfd9e
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-07-10 00:29:01 +0100

    added conditional ex

---
+++ 0809-c-c++/conditionalI.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    int x = 50;
+    int y = 50;
+
+    if (x ==  y) {
+        printf("1");
+    } 
+    else if
+    (x > y) {
+        printf("2");
+    } 
+    else
+    {
+        printf("3");
+    }
+
+    return 0;
+}
+
+
