483f5d5a4bd926cd3dc03b871cf930f55f7b6fd1
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-07-11 00:20:40 +0100

    added ended day

--- 0809-c-c++/aeroporto.c
+++ 0809-c-c++/aeroporto.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    int horaPartida;
-   
+    int horaPartida, minutoPartida, horaDuracao, minutoDuracao;
+    int horaChegada, minutoChegada, diaChegada;
 
     // Solicitar a hora de partida
     printf("AEROPORTO: INFORMAÇÕES DE EMBARQUE!\n");
     printf("Digite a hora de partida (0-23): ");
     scanf("%d", &horaPartida);
 
-    // Validar a hora de partida
-    if (horaPartida < 0 || horaPartida > 23) {
-        printf("Hora de partida inválida.\n");
-        return 0;
-    }
 
+
+    // Exibir a hora e o minuto de chegada
+    printf("\nAEROPORTO: DIA E HORA DE CHEGADA!\n");
+    printf("Hora de chegada: %02d:%02d\n", horaChegada, minutoChegada);
+
+    // Exibir se a chegada é no mesmo dia ou no dia seguinte
+    if (diaChegada == 0) {
+        printf("Chegada no mesmo dia.\n");
+    } else {
+        printf("Chegada no dia seguinte.\n");
+    }
 
 
     return 0;
