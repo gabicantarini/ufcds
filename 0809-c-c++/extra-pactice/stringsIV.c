49521283e40321f69a90cfe9492e227450d4b8af
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-07-11 17:07:12 +0100

    added first character

---
+++ 0809-c-c++/extra-pactice/stringsIV.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//Print the first character in the greetings string:
+
+int main() {
+
+    
+    char greetings[] = "Hello World!";
+    printf("%c", greetings[0]);
+
+    return 0;
+
+}
