676ee489a29c8cac64e8f1e7f1fa403563b8e561
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-07-11 16:49:40 +0100

    added for loop

---
+++ 0809-c-c++/extra-pactice/arrayForLoop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//Loop through the elements in the array using a for loop.
+int main() {
+
+    
+    int myNumbers[] = {25, 50, 75, 100};
+    int i;
+
+    for
+    (i = 0; i < 4; i++) {
+    printf("%d\n", myNumbers[i]);
+    }
+
+    return 0;
+
+}
