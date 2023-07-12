94afb447062e75428f7762071c7c7463b3448036
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-07-12 20:37:48 +0100

    added my function

---
+++ 0809-c-c++/extra-pactice/myFunction.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//Create a method named myFunction and call it inside main().
+
+void myFunction () {
+  printf("I just got executed!");
+}
+
+int main() {
+  
+    myFunction();
+
+    return 0;
+}
