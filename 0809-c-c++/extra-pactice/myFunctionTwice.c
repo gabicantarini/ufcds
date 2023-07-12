8bb6a90efcb9db1bdd50b1eb1ee6ed3741f287d9
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-07-12 20:37:58 +0100

    added my function twice

---
+++ 0809-c-c++/extra-pactice/myFunctionTwice.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//Insert the missing parts to call myFunction two times.
+
+void myFunction () {
+  printf("I just got executed!");
+}
+
+int main() {
+  
+    myFunction();
+    myFunction();
+
+    return 0;
+}
