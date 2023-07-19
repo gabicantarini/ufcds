5f0f8b62c4882d7e3fbb1614603c5e78350bf7d2
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-07-19 11:10:55 +0100

    add

--- 8016-Java/Exemplo9b/src/exemplo9b/Exemplo9b.java
+++ 8016-Java/Exemplo9b/src/exemplo9b/Exemplo9b.java
@@ -4,6 +4,7 @@
  */
 package exemplo9b;
 
+
 /**
  *
  * @author Gabriela
@@ -14,7 +15,18 @@ public class Exemplo9b {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Triangulo triangulo1 = new Triangulo();
+        TrianguloComCor triangulo1 = new TrianguloComCor();
+        
+        triangulo1.DefinirAltura(10);
+        triangulo1.DefinirBase(5);
+        triangulo1.DefinirCor("Yellow");
+        
+        System.out.println("Altura: " + triangulo1.ObterAltura());
+        System.out.println("Base: " + triangulo1.ObterBase());
+        System.out.println("Área: " + triangulo1.ObterArea());
+        System.out.println("Cor: " + triangulo1.ObterCor());
+        
     }
     
 }
