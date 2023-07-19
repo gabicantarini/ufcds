9ffa29bea3f9630bb73180afa61b2b22ab2cc44b
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-07-19 11:55:42 +0100

    update

--- 8016-Java/Exemplo9_classe/src/exemplo9_classe/Exemplo9_classe.java
+++ 8016-Java/Exemplo9_classe/src/exemplo9_classe/Exemplo9_classe.java
@@ -17,22 +17,22 @@ public class Exemplo9_classe {
         
            
         Bicicleta bike1= new Bicicleta();
-        bike1.DenifirCor("yellow");
-        bike1.DefinirNumeroDeRodas(2);
-        bike1.DefinirPeso(1.93);
+        bike1.setCor("yellow");
+        bike1.setNumeroDeRodas(2);
+        bike1.setPeso(1.93);
         
         
         Bicicleta bike2= new Bicicleta();
-        bike2.DenifirCor("Azul gira");
-        bike2.DefinirNumeroDeRodas(3);
-        bike2.DefinirPeso(1.68);
+        bike2.setCor("Azul gira");
+        bike2.setNumeroDeRodas(3);
+        bike2.setPeso(1.68);
         
-        System.out.println("A bicicleta 1 tem a cor: "+ bike1.ObterCor() + ", o número de rodas: " + bike1.ObterNumeroDeRodas() + " e o peso: "+bike1.ObterPeso()+".");
-        System.out.println("\n A bicicleta 2 tem a cor: "+ bike2.ObterCor() + ", o número de rodas: " + bike2.ObterNumeroDeRodas() + " e o peso: "+bike2.ObterPeso()+".");
+        System.out.println("A bicicleta 1 tem a cor: "+ bike1.getCor() + ", o número de rodas: " + bike1.setNumeroDeRodas() + " e o peso: "+bike1.setPeso()+".");
+        System.out.println("\n A bicicleta 2 tem a cor: "+ bike2.getCor() + ", o número de rodas: " + bike2.setNumeroDeRodas() + " e o peso: "+bike2.setPeso()+".");
     
         BicicletaComMotor bike3=new BicicletaComMotor();
-        bike3.DenifirCor("green");
-        bike3.DefinirNumeroDeRodas(3);
+        bike3.setCor("green");
+        bike3.setNumeroDeRodas(3);
         bike3.motor = "Suzuki";
         
         bike3.MostrarMotor();
--- 8016-Java/Exemplo9_classe/src/veiculos/Bicicleta.java
+++ 8016-Java/Exemplo9_classe/src/veiculos/Bicicleta.java
@@ -12,30 +12,40 @@ public class Bicicleta {
     private String cor;
     private int numeroDeRodas;
     private double peso;
-    
-    public String ObterCor(){
+
+    public String getCor() {
         return cor;
     }
-    
-    public void DenifirCor(String c){
-        cor=c;
+
+    public void setCor(String cor) {
+        this.cor = cor;
     }
-    
-    public int ObterNumeroDeRodas(){
+
+    public int getNumeroDeRodas() {
         return numeroDeRodas;
     }
-     
-    public int DefinirNumeroDeRodas(int d){
-        return numeroDeRodas=d;
+
+    public void setNumeroDeRodas(int numeroDeRodas) {
+        this.numeroDeRodas = numeroDeRodas;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public String setPeso() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String setNumeroDeRodas() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
-    public double ObterPeso(){
-        return peso;
-    } 
-    
-    public double DefinirPeso(double p){
-        return peso = p;
-    } 
+   
           
     
 }
