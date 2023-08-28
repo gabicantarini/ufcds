6449f9bdbb96c9fbe59216fddff664f4bb92be7b
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-08-28 09:26:23 +0100

    add

--- 8016-Java/Exemplo9_classe/src/exemplo9_classe/Exemplo9_classe.java
+++ 8016-Java/Exemplo9_classe/src/exemplo9_classe/Exemplo9_classe.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package exemplo9_classe;
+import veiculos.Bicicleta;
+
 
 /**
  *
@@ -10,11 +12,22 @@ package exemplo9_classe;
  */
 public class Exemplo9_classe {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+       public static void main(String[] args) {
+        
+        Bicicleta bike1= new Bicicleta();
+        bike1.cor = "yellow";
+        bike1.numeroDeRodas = 2;
+        bike1.peso = 2.23;
+        
+        Bicicleta bike2= new Bicicleta();
+        bike2.cor = "Azul gira";
+        bike2.numeroDeRodas = 3;
+        bike2.peso = 1.68;
+        
+        System.out.println("A bicicleta 1 tem a cor: "+bike1.cor + ", o número de rodas: " + bike1.numeroDeRodas+ " e o peso: "+bike1.peso+".");
+        System.out.println("\n A bicicleta 2 tem a cor: "+bike2.cor + ", o número de rodas: " + bike2.numeroDeRodas+ " e o peso: "+bike2.peso+".");
+    
+     
     }
     
 }
