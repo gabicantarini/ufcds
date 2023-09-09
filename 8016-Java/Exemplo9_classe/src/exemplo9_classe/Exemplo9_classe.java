46e0d87bc682598c9e6f116299c8967a1925f12b
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-09-09 11:20:07 +0100

    add

--- 8016-Java/Exemplo9_classe/src/exemplo9_classe/Exemplo9_classe.java
+++ 8016-Java/Exemplo9_classe/src/exemplo9_classe/Exemplo9_classe.java
@@ -5,7 +5,7 @@
 package exemplo9_classe;
 import veiculos.Bicicleta;
 import veiculos.BicicletaComMotor;
-import veiculos.Tractor;
+//import veiculos.Tractor;
 
 /**
  *
@@ -15,28 +15,30 @@ public class Exemplo9_classe {
 
        public static void main(String[] args) {
         
+           
         Bicicleta bike1= new Bicicleta();
-        bike1.cor = "yellow";
-        bike1.numeroDeRodas = 2;
-        bike1.peso = 2.23;
+        bike1.DenifirCor("yellow");
+        bike1.DefinirNumeroDeRodas(2);
+        bike1.DefinirPeso(1.93);
+        
         
         Bicicleta bike2= new Bicicleta();
-        bike2.cor = "Azul gira";
-        bike2.numeroDeRodas = 3;
-        bike2.peso = 1.68;
+        bike2.DenifirCor("Azul gira");
+        bike2.DefinirNumeroDeRodas(3);
+        bike2.DefinirPeso(1.68);
         
-        System.out.println("A bicicleta 1 tem a cor: "+bike1.cor + ", o número de rodas: " + bike1.numeroDeRodas+ " e o peso: "+bike1.peso+".");
-        System.out.println("\n A bicicleta 2 tem a cor: "+bike2.cor + ", o número de rodas: " + bike2.numeroDeRodas+ " e o peso: "+bike2.peso+".");
+        System.out.println("A bicicleta 1 tem a cor: "+ bike1.ObterCor() + ", o número de rodas: " + bike1.ObterNumeroDeRodas() + " e o peso: "+bike1.ObterPeso()+".");
+        System.out.println("\n A bicicleta 2 tem a cor: "+ bike2.ObterCor() + ", o número de rodas: " + bike2.ObterNumeroDeRodas() + " e o peso: "+bike2.ObterPeso()+".");
     
         BicicletaComMotor bike3=new BicicletaComMotor();
-        bike3.cor = "green";
+        bike3.DenifirCor("green");
+        bike3.DefinirNumeroDeRodas(3);
         bike3.motor = "Suzuki";
         
         bike3.MostrarMotor();
-        bike3.MostrarCor();
+        //bike3.MostrarCor();
         
-        bike4.
-    
+            
     }
     
 }
