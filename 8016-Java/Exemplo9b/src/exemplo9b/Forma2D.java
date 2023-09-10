2fd4ef2d6e182e47d54d4ca0d2bbeeab5acc4171
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-09-10 12:16:22 +0100

    add exemplo9b

---
+++ 8016-Java/Exemplo9b/src/exemplo9b/Forma2D.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exemplo9b;
+
+/**
+ *
+ * @author Gabriela
+ */
+public class Forma2D {
+    
+}
