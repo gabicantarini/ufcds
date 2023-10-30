769f415ebece50ffdd7a809b27ba8cb475d51b35
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-10-30 11:36:37 +0000

    add pallet

---
+++ 8016-Java/Exemplo15_pallete/src/exemplo15_pallete/Exemplo15_pallete.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exemplo15_pallete;
+
+/**
+ *
+ * @author Gabriela
+ */
+public class Exemplo15_pallete {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Classe_Janela f1;
+        f1=new Classe_Janela();
+        f1.setVisible(true);
+    }
+    
+}
