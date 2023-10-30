f3d7cec2df9092758a204bf9404f461e5a34f4be
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-10-30 11:36:11 +0000

    add conversor

---
+++ 8016-Java/Exemplo14_conversor/src/exemplo14_conversor/Exemplo14_conversor.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exemplo14_conversor;
+
+/**
+ *
+ * @author Gabriela
+ */
+public class Exemplo14_conversor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Classe_Janela f1;
+        f1=new Classe_Janela();
+        f1.setVisible(true);
+    }
+    
+}
