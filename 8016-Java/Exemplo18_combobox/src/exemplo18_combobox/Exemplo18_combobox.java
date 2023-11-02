b8cf4a5a5d6bef35b8c54c24ec2755c72775f5a3
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-11-02 11:32:44 +0000

    add combobox

---
+++ 8016-Java/Exemplo18_combobox/src/exemplo18_combobox/Exemplo18_combobox.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exemplo18_combobox;
+
+/**
+ *
+ * @author Gabriela
+ */
+public class Exemplo18_combobox {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Classo_combobox f1;
+        f1=new Classo_combobox();
+        f1.setVisible(true); // TODO code application logic here
+    }
+    
+}
