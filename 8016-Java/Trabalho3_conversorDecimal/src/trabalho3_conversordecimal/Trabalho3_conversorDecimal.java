7e77984c204fae88c6faf8832d9711695c8b0026
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-11-02 11:33:25 +0000

    add conversor

---
+++ 8016-Java/Trabalho3_conversorDecimal/src/trabalho3_conversordecimal/Trabalho3_conversorDecimal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trabalho3_conversordecimal;
+
+/**
+ *
+ * @author Gabriela
+ */
+public class Trabalho3_conversorDecimal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Classe_Janela f1;
+        f1=new Classe_Janela();
+        f1.setVisible(true);
+    }
+    
+}
