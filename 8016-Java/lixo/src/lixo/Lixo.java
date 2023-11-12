3fe570d4a754addb5cb928d7265f2756e3f45062
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-11-12 12:09:53 +0000

    add java test

---
+++ 8016-Java/lixo/src/lixo/Lixo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lixo;
+
+/**
+ *
+ * @author Gabriela
+ */
+public class Lixo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        lixo1 f1;
+        f1=new lixo1();
+        f1.setVisible(true);
+    }
+    
+}
