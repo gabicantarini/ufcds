906b73b151c0a892ea052a19774e3213108155c6
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-12-04 13:18:13 +0000

    add controles

---
+++ 3934-Visual-Basic/diversosControlos/Form1.Designer.vb
@@ -0,0 +1,290 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.ButtonQuadrado = New System.Windows.Forms.Button()
+        Me.ButtonRetangulo = New System.Windows.Forms.Button()
+        Me.ButtonTriangulo = New System.Windows.Forms.Button()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(54, 110)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(104, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Comprimento"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(54, 167)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Área"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(54, 235)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(64, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Largura"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(54, 292)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(51, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Altura"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(58, 348)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(46, 20)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Base"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(263, 25)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(199, 32)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Calcular Área"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(228, 103)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(201, 26)
+        Me.TextBox1.TabIndex = 6
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(228, 160)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(201, 26)
+        Me.TextBox2.TabIndex = 7
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(228, 228)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(201, 26)
+        Me.TextBox3.TabIndex = 8
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(228, 292)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(201, 26)
+        Me.TextBox4.TabIndex = 9
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(228, 348)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(201, 26)
+        Me.TextBox5.TabIndex = 10
+        '
+        'ButtonQuadrado
+        '
+        Me.ButtonQuadrado.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
+        Me.ButtonQuadrado.Location = New System.Drawing.Point(542, 90)
+        Me.ButtonQuadrado.Name = "ButtonQuadrado"
+        Me.ButtonQuadrado.Size = New System.Drawing.Size(144, 53)
+        Me.ButtonQuadrado.TabIndex = 11
+        Me.ButtonQuadrado.Text = "Calcular Quadrado"
+        Me.ButtonQuadrado.UseVisualStyleBackColor = True
+        '
+        'ButtonRetangulo
+        '
+        Me.ButtonRetangulo.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
+        Me.ButtonRetangulo.Location = New System.Drawing.Point(542, 201)
+        Me.ButtonRetangulo.Name = "ButtonRetangulo"
+        Me.ButtonRetangulo.Size = New System.Drawing.Size(144, 53)
+        Me.ButtonRetangulo.TabIndex = 12
+        Me.ButtonRetangulo.Text = "Calcular Retângulo"
+        Me.ButtonRetangulo.UseVisualStyleBackColor = True
+        '
+        'ButtonTriangulo
+        '
+        Me.ButtonTriangulo.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
+        Me.ButtonTriangulo.Location = New System.Drawing.Point(542, 315)
+        Me.ButtonTriangulo.Name = "ButtonTriangulo"
+        Me.ButtonTriangulo.Size = New System.Drawing.Size(144, 53)
+        Me.ButtonTriangulo.TabIndex = 13
+        Me.ButtonTriangulo.Text = "Calcular Triângulo"
+        Me.ButtonTriangulo.UseVisualStyleBackColor = True
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(901, 104)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(122, 26)
+        Me.TextBox6.TabIndex = 14
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(749, 109)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(118, 20)
+        Me.Label7.TabIndex = 17
+        Me.Label7.Text = "Área Quadrado"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(749, 217)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(118, 20)
+        Me.Label8.TabIndex = 19
+        Me.Label8.Text = "Área Quadrado"
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(901, 212)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(122, 26)
+        Me.TextBox7.TabIndex = 18
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(749, 331)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(118, 20)
+        Me.Label9.TabIndex = 21
+        Me.Label9.Text = "Área Quadrado"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(901, 326)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(122, 26)
+        Me.TextBox8.TabIndex = 20
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Location = New System.Drawing.Point(269, 461)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(578, 353)
+        Me.GroupBox1.TabIndex = 22
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Dados do Cálculo"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(269, 435)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(0, 20)
+        Me.Label10.TabIndex = 23
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1153, 876)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.ButtonTriangulo)
+        Me.Controls.Add(Me.ButtonRetangulo)
+        Me.Controls.Add(Me.ButtonQuadrado)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents ButtonQuadrado As Button
+    Friend WithEvents ButtonRetangulo As Button
+    Friend WithEvents ButtonTriangulo As Button
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label10 As Label
+End Class
---
+++ 3934-Visual-Basic/diversosControlos/Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub ButtonQuadrado_Click(sender As Object, e As EventArgs) Handles ButtonQuadrado.Click
+        Dim C As Double ' define a variável do comprimento
+        Dim A1 As Double ' define a variável daárea
+        C = TextBox1.Text ' Obter o comprimento.
+        A1 = C ^ 2 ' Calculara area
+        TextBox6.Text = A1 ' mostra aárea na segundatext box
+        ' ListBox1.Items.Add("A área do Quadrado é:" & A1) ' adiciona à listbox
+    End Sub
+
+    Private Sub ButtonRetangulo_Click(sender As Object, e As EventArgs) Handles ButtonRetangulo.Click
+        Dim L As Double ' ' define a variável do Largura
+        Dim A As Double ' ' define a variável do Altura
+        Dim A2 As Double ' define aarea
+        L = TextBox3.Text ' obtém a Largura
+        A = TextBox4.Text ' obtém aAltura
+        A2 = L * A ' Calcula a área
+        TextBox7.Text = A2 ' mostra aáreado retângulo
+        ' ListBox1.Items.Add(S2) ' adiciona a mensagemna list box
+    End Sub
+
+    Private Sub ButtonTriangulo_Click(sender As Object, e As EventArgs) Handles ButtonTriangulo.Click
+        Dim base As Double ' define abase
+        Dim altura As Double ' define a Altura
+        Dim Area As Double ' define aarea
+        base = TextBox5.Text ' obtém base
+        altura = TextBox4.Text ' obtém a Altura
+        Area = 0.5 * base * altura ' Calcula a area(1/2 x base x altura)
+        TextBox8.Text = Area ' mostra o resultado.
+        ' ListBox1.Items.Add(Str) ' adiciona a mensagemna list box
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+        TextBox1.Text = "" ' Limpar todas as text boxes
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+        TextBox6.Text = ""
+        TextBox7.Text = ""
+        TextBox8.Text = ""
+        ' ListBox1.Items.Clear() ' Limpar  a list box
+    End Sub
+End Class
