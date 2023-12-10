d5d258af17d1ff74bb76ca4032c50c4a10e9aa05
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-12-10 10:35:53 +0000

    added piano

---
+++ 10791-Desenvolvimento-app-web-Java/piano-tradicional/src/js/piano.js
@@ -0,0 +1,131 @@
+
+function doNota() {
+    let doNota = document.getElementById('do');
+    let button = document.getElementById('do-button');
+    button.style.backgroundColor = "blue";
+    button.style.color = "white";
+    doNota.onpause;
+    doNota.currentTime = 0;
+    doNota.play();
+}
+
+function reNota() {
+    let reNota = document.getElementById('re');
+    let button = document.getElementById('re-button');
+    button.style.backgroundColor = "green";
+    button.style.color = "white";
+    reNota.onpause;
+    reNota.currentTime = 0;
+    reNota.play();
+}ss
+
+function miNota() {
+    let miNota = document.getElementById('mi');
+    let button = document.getElementById('mi-button');
+    button.style.backgroundColor = "orange";
+    button.style.color = "white";
+    miNota.onpause;
+    miNota.currentTime = 0;
+    miNota.play();
+}
+function faNota() {
+    let faNota = document.getElementById('fa');
+    let button = document.getElementById('fa-button');
+    button.style.backgroundColor = "pink";
+    button.style.color = "white";
+    faNota.onpause;
+    faNota.currentTime = 0;
+    faNota.play();
+}
+function solNota() {
+    let solNota = document.getElementById('sol');
+    let button = document.getElementById('sol-button');
+    button.style.backgroundColor = "green";
+    button.style.color = "white";
+    solNota.onpause;
+    solNota.currentTime = 0;
+    solNota.play();
+}
+function laNota() {
+    let laNota = document.getElementById('la');
+    let button = document.getElementById('la-button');
+    button.style.backgroundColor = "purple";
+    button.style.color = "white";
+    laNota.onpause;
+    laNota.currentTime = 0;
+    laNota.play();
+}
+function siNota() {
+    let siNota = document.getElementById('si');
+    let button = document.getElementById('si-button');
+    button.style.backgroundColor = "brown";
+    button.style.color = "white";
+    siNota.onpause;
+    siNota.currentTime = 0;
+    siNota.play();
+}
+function houseNota() {
+    let siNota = document.getElementById('house');
+    let button = document.getElementById('house-button');
+    button.style.backgroundColor = "yellow";
+    button.style.color = "white";
+    siNota.onpause;
+    siNotacurrentTime = 0;
+    siNota.play();
+}
+function ohnoNota() {
+    let ohnoNota = document.getElementById('ohno');
+    let button = document.getElementById('ohno-button');
+    button.style.backgroundColor = "violet";
+    button.style.color = "white";
+    ohnoNota.onpause;
+    ohnoNota.currentTime = 0;
+    ohnoNota.play();
+}
+function loveNota() {
+    let loveNota = document.getElementById('love');
+    let button = document.getElementById('love-button');
+    button.style.backgroundColor = "red";
+    button.style.color = "white";
+    loveNota.onpause;
+    loveNota.currentTime = 0;
+    loveNota.play();
+}
+
+function allAboardNota() {
+    let allaboard = document.getElementById('allaboard');
+    let button = document.getElementById('allAboard-button');
+    button.style.backgroundColor = "blue";
+    button.style.color = "white";
+    allaboard.onpause;
+    allaboard.currentTime = 0;
+    allaboard.play();
+}
+
+function fernandoNota() {
+    let fernando = document.getElementById('fernando');
+    let button = document.getElementById('fernando-button');
+    button.style.backgroundColor = "grey";
+    button.style.color = "white";
+    fernando.onpause;
+    fernando.currentTime = 0;
+    fernando.play();
+}
+
+function MexeOMingau(event) {
+    var key = event.keyCode;
+    if (key == 76) //tecla L
+        doNota();
+    if (key == 83)//Tecla do S
+        renota();
+    if (key == 68)//Tecla do D
+        minota();
+    if (key == 70)//Tecla do F
+        fanota();
+    if (key == 71)//Tecla do G
+        sonota();
+    if (key == 72)//Tecla do H
+        lanota();
+    if (key == 74)//Tecla do J
+        sinota();
+}
---
+++ 10791-Desenvolvimento-app-web-Java/piano/src/js/piano.js
@@ -0,0 +1,75 @@
+
+function doNota(){
+    var doNota = document.getElementById('do');
+    var button = document.getElementById('do-button');
+    button.style.backgroundColor = "blue";
+    button.style.color = "white";
+    doNota.onpause;
+    doNota.currentTime=0;
+    doNota.play();
+}
+
+function reNota(){
+    var reNota = document.getElementById('re');
+    var button = document.getElementById('re-button');
+    button.style.backgroundColor = "blue";
+    button.style.color = "white";
+    reNota.onpause;
+    reNota.currentTime=0;
+    reNota.play();
+}
+
+function miNota(){
+    var miNota = document.getElementById('mi');
+    miNota.onpause;
+    miNota.currentTime=0;
+    miNota.play();
+}
+function faNota(){
+    document.getElementById('fa').onpause;
+    document.getElementById('fa').currentTime=0;
+    document.getElementById('fa').play();
+}
+function solNota(){
+    document.getElementById('sol').onpause;
+    document.getElementById('sol').currentTime=0;
+    document.getElementById('sol').play();
+}
+function laNota(){
+    document.getElementById('la').onpause;
+    document.getElementById('la').currentTime=0;
+    document.getElementById('la').play();
+}
+function siNota(){
+    document.getElementById('si').onpause;
+    document.getElementById('si').currentTime=0;
+    document.getElementById('si').play();
+}
+function houseNota(){
+    document.getElementById('house').onpause;
+    document.getElementById('house').currentTime=0;
+    document.getElementById('house').play();
+}
+function ohnoNota(){
+    document.getElementById('ohno').onpause;
+    document.getElementById('ohno').currentTime=0;
+    document.getElementById('ohno').play();
+}
+function loveNota(){
+    document.getElementById('love').onpause;
+    document.getElementById('love').currentTime=0;
+    document.getElementById('love').play();
+}
+
+function allAboardNota(){
+    document.getElementById('allaboard').onpause;
+    document.getElementById('allaboard').currentTime=0;
+    document.getElementById('allaboard').play();
+}
+
+function fernandoNota(){
+    document.getElementById('fernando').onpause;
+    document.getElementById('fernando').currentTime=0;
+    document.getElementById('fernando').play();
+}
+
