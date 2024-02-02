cfe1044bcd3b33b20f792d7427bb595c06db3aff
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-02-02 16:26:40 +0000

    updated trimestres

--- 10793-Python/exercicio4_GabrielaCantarini.py
+++ 10793-Python/exercicio4_GabrielaCantarini.py
@@ -1,5 +1,5 @@
 # Dicionário 
-meses = {
+MESES = {
     1: 'Janeiro',
     2: 'Fevereiro',
     3: 'Março',
@@ -19,12 +19,12 @@ trimestre = int(input("Digite o número do trimestre [1 a 4]: "))
 
 # Retorna o conjunto de meses ao trimestre conforme solicitado pelo usuário
 if trimestre == 1:
-    print("%s, %s, %s" % ("O trimestre digitado é: " + meses.get(1), meses.get(2), meses.get(3)))
+    print("%s, %s, %s" % ("O trimestre digitado é: " + MESES.get(1), MESES.get(2), MESES.get(3)))
 elif trimestre == 2:
-    print("%s, %s, %s" % ("O trimestre digitado é: " + meses.get(4), meses.get(5), meses.get(6)))
+    print("%s, %s, %s" % ("O trimestre digitado é: " + MESES.get(4), MESES.get(5), MESES.get(6)))
 elif trimestre == 3:
-    print("%s, %s, %s" % ("O trimestre digitado é: " + meses.get(7), meses.get(8), meses.get(9)))
+    print("%s, %s, %s" % ("O trimestre digitado é: " + MESES.get(7), MESES.get(8), MESES.get(9)))
 elif trimestre == 4:
-    print("%s, %s, %s" % ("O trimestre digitado é: " + meses.get(10), meses.get(11), meses.get(12)))
+    print("%s, %s, %s" % ("O trimestre digitado é: " + MESES.get(10), MESES.get(11), MESES.get(12)))
 else:
     print("Número do trimestre inválido. Por favor, digite um número entre 1 e 4.")
