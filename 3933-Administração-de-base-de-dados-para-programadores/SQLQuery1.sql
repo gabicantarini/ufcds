e497ec113c71b6bf516e9ab7175dc5e886ecdf8a
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-03-31 22:55:27 +0100

    added query

---
+++ 3933-Administração-de-base-de-dados-para-programadores/SQLQuery1.sql
@@ -0,0 +1,16 @@
+create database gestaoformandos;
+ 
+use gestaoformandos;
+ 
+create table formando (
+    id_formando int,
+    nome varchar(100) not null,
+    morada varchar(100) not null,
+    contacto varchar(9) null,
+    iban varchar(50) not null default '0',
+    sexo char(1) not null,
+    data_nascimento date not null
+);
+ 
+insert into formando (id_formando, nome, morada, contacto, iban, sexo, data_nascimento) values
+(1, 'Ana Silva', 'Coimbra', null, '0', 'F', '2000-12-31');
